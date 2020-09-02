d154f47366e6db50af53bc4e8746977de753dc41
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-02 13:25:26 +0530

    Created the playerSchema

--- models/playerModels.js
+++ models/playerModels.js
@@ -2,3 +2,17 @@ const { mongo } = require("mongoose");
 
 const mangoose=require("mongoose");
 
+const playerSchema=new mongoose.Schema({
+
+    firstName:{type:String,required:true},
+    lastName:{type:String,required:true},
+    email:{type:String,required:true},
+    phone:{type:Number,required:true}   
+
+})
+
+
+
+
+
+module.exports=mongoose.model("",playerSchema);
